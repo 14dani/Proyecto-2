4f666dd357787272deaef09e02deea513cf801d2
Esteban López <cestebanls@hotmail.com>
2018-11-13 21:52:15 -0600

    .

--- Proyecto2/Proyecto2/Monitoreo.cpp
+++ Proyecto2/Proyecto2/Monitoreo.cpp
@@ -6,7 +6,10 @@
 #include <iomanip>
 using namespace std;
 
-
+/*Supuestamente la pagina se tiene que actualizar al apretar cierto comando,
+lo que se me ocurre es que siempre que se ingrese al comando de monitoreo el programa cargue automaticamente la lista.
+Le colocamos un switch al puro inicio, si el swicth esta encendido quiere decir que el programa se reinicio para refrescar la lista.
+Si el interruptor switch esta apagado quiere decir que el programa de monitoreo se esta iniciando normalmente*/
 
 Monitoreo::Monitoreo()
 {
